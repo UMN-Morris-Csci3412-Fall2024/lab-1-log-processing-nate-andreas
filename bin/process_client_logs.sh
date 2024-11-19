21d847a771db80bbb3e5a3d49eaa1bb07cd965c3
AndreasNelson <nel02254@morris.umn.edu>
2024-11-19 13:10:37 -0600

    merged all changes from test fix branch

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -11,16 +11,24 @@ if [ ! -d "$1" ] || [ -z "$(ls -A "$1")" ]; then
   exit 1
 fi
 
-# Process the log files
+# Find all files in the specified directory and concatenate their contents
 find "$1" -type f -exec cat {} + | awk '
+  # Process lines with "Failed password for invalid user"
   /Failed password for invalid user/ {
+    # Split the line into an array 'a' using space as the delimiter
     split($0, a, " ")
+    # Split the time field (a[3]) into an array 't' using colon as the delimiter
     split(a[3], t, ":")
+    # Print the date, month, hour (dropping minutes and seconds), username, and IP address
     print a[1], a[2], t[1], a[11], a[13]
   }
+  # Process lines with "Failed password for" but exclude "invalid user" and "Accepted password"
   /Failed password for / && !/invalid user/ && !/Accepted password/ {
+    # Split the line into an array 'a' using space as the delimiter
     split($0, a, " ")
+    # Split the time field (a[3]) into an array 't' using colon as the delimiter
     split(a[3], t, ":")
+    # Print the date, month, hour (dropping minutes and seconds), username, and IP address
     print a[1], a[2], t[1], a[9], a[11]
   }
-' > "$1/failed_login_data.txt"
+' > "$1/failed_login_data.txt"  # Redirect the output to the specified file
