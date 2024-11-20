c4f798ab1e6ec15f196318c028830a5ad07f8907
AndreasNelson <nel02254@morris.umn.edu>
2024-11-20 13:58:47 -0600

    fixed shellcheck error

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -13,22 +13,18 @@ fi
 
 # Find all files in the specified directory and concatenate their contents
 find "$1" -type f -exec cat {} + | awk '
-  # Process lines with "Failed password for invalid user"
+  # Split the line into an array "a" using space as the delimiter
   /Failed password for invalid user/ {
-    # Split the line into an array 'a' using space as the delimiter
     split($0, a, " ")
-    # Split the time field (a[3]) into an array 't' using colon as the delimiter
+    # Split the time field (a[3]) into an array "t" using colon as the delimiter
     split(a[3], t, ":")
-    # Print the date, month, hour (dropping minutes and seconds), username, and IP address
     print a[1], a[2], t[1], a[11], a[13]
   }
-  # Process lines with "Failed password for" but exclude "invalid user" and "Accepted password"
+  # Split the line into an array "a" using space as the delimiter
   /Failed password for / && !/invalid user/ && !/Accepted password/ {
-    # Split the line into an array 'a' using space as the delimiter
     split($0, a, " ")
-    # Split the time field (a[3]) into an array 't' using colon as the delimiter
+    # Split the time field (a[3]) into an array "t" using colon as the delimiter
     split(a[3], t, ":")
-    # Print the date, month, hour (dropping minutes and seconds), username, and IP address
     print a[1], a[2], t[1], a[9], a[11]
   }
-' > "$1/failed_login_data.txt"  # Redirect the output to the specified file
+' > "$1/failed_login_data.txt"
